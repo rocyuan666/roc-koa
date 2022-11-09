db0e6eee6b46d59c9000557fe011c2a81cd0d5cb
袁鹏 <rocyuan666@163.com>
2022-11-09 12:25:34 +0800

    jwt

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,9 @@
 const userService = require("../service/user.service");
-const { apiSuccess } = require("../utils/apiBase");
 
 class UserController {
   async findById(ctx, next) {
     const { userId } = ctx.params;
-    const result = await userService.getUserById(userId);
-    ctx.body = apiSuccess(result);
+    ctx.body = await userService.getUserById(userId);
   }
 }
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,11 +1,11 @@
-const connection = require("../app/database");
+const db = require("../app/database");
+const { apiSuccess } = require("../utils/apiBase");
 
 class UserService {
   async getUserById(userId) {
-    const statement = `SELECT * FROM sys_user WHERE user_id = ?;`;
-    const result = await connection.execute(statement, [userId]);
-
-    return result[0];
+    const sql = `SELECT * FROM sys_user WHERE user_id = ?;`;
+    const [result] = await db.execute(sql, [userId]);
+    return apiSuccess(result);
   }
 }
 
